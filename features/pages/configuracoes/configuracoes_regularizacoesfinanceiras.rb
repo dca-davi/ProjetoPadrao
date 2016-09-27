b87136bed005d314b0e0d3c574e4ac7af8b5995d
Demis Meneghetti <demismeneghetti@msn.com>
2016-09-27 08:19:09 -0300

    Atualização de projeto.

---
+++ features/pages/configuracoes/configuracoes_regularizacoesfinanceiras.rb
@@ -0,0 +1,15 @@
+# CONFIGURACOES_REGULARIZACOESFINANCEIRAS_REGRASDECANCELAMENTO_VER
+# CONFIGURACOES_REGULARIZACOESFINANCEIRAS_REGRADELIBERACAO_VER
+class Configuracoes_regularizaoesfinanceiras
+    # Demis >>>
+    def validar_painel_grid(_grid)
+        if $browser.span(text: 'Dados da pesquisa').exists?
+            $encoded_img = $browser.driver.screenshot_as(:base64)
+            return true
+        else
+            $encoded_img = $browser.driver.screenshot_as(:base64)
+            return false
+        end
+    end
+    # Demis <<<
+end
