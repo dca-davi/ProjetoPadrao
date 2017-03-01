c3229b4ec6d0b1cf6e64d1bba0e5025dd17915e0
rafaponzio@gmail.com <Rafael.ponzio@dynatec.com.br>
2017-03-01 11:58:51 -0300

    Update

--- gerar_run_por_status.rb
+++ gerar_run_por_status.rb
@@ -17,6 +17,7 @@ book = Spreadsheet.open('./features/Dados-CTs-Atualizar-Status.xls')
 sheet1 = book.worksheet 0
 column_counter = sheet1.column_count
 row_counter = sheet1.row_count
+count = 1
 
 for count_row in 1...row_counter
     for count_column in 1...column_counter
@@ -29,7 +30,16 @@ for count_row in 1...row_counter
     status_cenario = sheet1.cell(count_row, 5).to_s
 
     $rest_ALM.obter_dados_ALM(release, testset, ciclo, nome_cenario)
-    $rest_ALM.alterar_status_teste_ALM(status_cenario)
+    status_teste = $rest_ALM.checar_status_ALM
+    case status_teste
+        when 'Failed'
+            #$rest_ALM.alterar_status_teste_ALM(status_cenario)
+            puts  "#{count} - #{nome_cenario} - Atual: #{status_teste} | Novo: #{status_cenario}"
+        else
+            puts "#{count} - #{nome_cenario} Atual: #{status_teste} | Não Alterável para: #{status_cenario}"            
+    end
+
+    count += 1        
 end
 
 $rest_ALM.desconectar_ALM
