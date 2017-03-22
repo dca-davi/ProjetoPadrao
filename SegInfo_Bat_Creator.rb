8fba61454831824479fddaeb440d1c493f38c6d5
Davi Cordeiro Alves <davi.alves@spread.tecnologia>
2017-03-22 16:12:02 -0300

    projeto

--- SegInfo_Bat_Creator.rb
+++ SegInfo_Bat_Creator.rb
@@ -39,7 +39,7 @@ for count in 1...perfis.row_count               # Percorrer todos os perfis da p
         file << "chcp 65001\n"
         file << "cd C:\\Users\\walter.castanharo\\SEgInfo\\automation-test-seginfo\n"
         file << "echo Executando Perfil #{perfil} - Direito #{direito}\n"
-		file << "cucumber --tag @#{direito} Perfil=#{perfil} --format html --out=\"#{report_name}\" TEST_ENV=#{ambiente} RELEASE_ALM=\"[HML] Homologação Ciclo 1\" CICLO_ALM=\"[Automação] Prevenção e Segurança\" TESTSET_ALM=\"#{testset}\" EVIDENCIA_ALM=S CAMINHO_LOG_EXECUCAO=\"#{nome_arquivo_log}\" SOBRESCREVE_REGISTRO_LOG=S EXECUTAR_STATUS=\"Failed\n"
+		file << "cucumber --tag @#{direito} Perfil=#{perfil} --format html --out=\"#{report_name}\" TEST_ENV=#{ambiente} RELEASE_ALM=\"[HML] Homologação Ciclo 1\" CICLO_ALM=\"[Automação] Prevenção e Segurança\" TESTSET_ALM=\"#{testset}\" EVIDENCIA_ALM=S CAMINHO_LOG_EXECUCAO=\"#{nome_arquivo_log}\" SOBRESCREVE_REGISTRO_LOG=S EXECUTAR_STATUS=\"\"\n"
         file << "cd #{dirname}\n"
         file.close
 
